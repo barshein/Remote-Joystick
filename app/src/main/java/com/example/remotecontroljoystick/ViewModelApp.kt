dba3515ab87679be4d6bdf5c77f8e880c697695d
ShellyNR <shelly.nahir@live.biu.ac.il>
2021-06-19 16:30:52 +0300

    Second commit

---
+++ app/src/main/java/com/example/remotecontroljoystick/ViewModelApp.kt
@@ -0,0 +1,28 @@
+package com.example.remotecontroljoystick
+
+import androidx.lifecycle.ViewModel
+import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class viewModel : ViewModel() {
+/**
+    // The IP and Port properties
+    private var _editPort = MutableLiveData<Int>()
+    private var _editIP = MutableLiveData<Int>()
+
+
+    val editPort: LiveData<Int>
+        get() = _editPort
+    }
+
+    val editIP: LiveData<Int>
+        get() = _editIP
+    }
+
+
+**/
+
+
+
+}
