96e36c0056f3fd921609ee56df8b618b7da66355
barshein <82324960+barshein@users.noreply.github.com>
2021-06-27 15:06:02 +0300

    Update ViewModelApp.kt

--- app/src/main/java/com/example/remotecontroljoystick/ViewModelApp.kt
+++ app/src/main/java/com/example/remotecontroljoystick/ViewModelApp.kt
@@ -8,49 +8,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class ViewModelApp : ViewModel() {
-    /**
-    // The IP and Port properties
-    private var _editPort = MutableLiveData<Int>()
-    private var _editIP = MutableLiveData<Int>()
-
-
-    val editPort: LiveData<Int>
-    get() = _editPort
-    }
-
-    val editIP: LiveData<Int>
-    get() = _editIP
-    }
-
-
-     **/
-
-    val vmIP = MutableLiveData<String>()
-    val vmPort = MutableLiveData<Int>()
-
-    fun onClick(){
-        println("hello")
-        println()
-        val ip:String = vmIP.getValue().toString()
-        val port = vmPort.getValue()
-        println("i")
-        if (ip != null) {
-            println("p")
-        }
-        Model.initModel("ip",800)
-
-    }
-
-
-    val _isOn = MutableLiveData<String>()
-
-
-
-
-    val isOn: LiveData<String>
-        get() = Model.Current
-
-
 
 }
 
